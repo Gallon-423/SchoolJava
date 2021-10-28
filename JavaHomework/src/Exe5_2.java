7458304f47636c649345a77ba8f4df55946ecd47
ChenJialong <2833275565@qq.com>
2021-10-28 16:45:42 +0800

    10.28 Some modification.

--- JavaHomework/src/Exe5_2.java
+++ JavaHomework/src/Exe5_2.java
@@ -1,11 +1,23 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exe5_2 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String str=sc.nextLine();
-        System.out.println(wordCount(str));
+        System.out.println("输入句子的单词数为："+wordCount(str));
         System.out.println(str);
+
+        //以下为更简便实现方法，未提交至邮箱，没必要
+        String[] s=str.split(" ");
+        int count=0;
+        for (String a:s
+             ) {
+            if(a.matches("^[A-Z.a-z]+$"))
+            count++;
+
+        }
+        System.out.println("输入句子的单词数为："+count);
     }
     static int wordCount(String str){
         char[] chars=str.toCharArray();
