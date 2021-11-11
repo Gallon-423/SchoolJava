52c0a951a73a5eecebb8a168c7b6cb9a1a3e851f
ChenJialong <2833275565@qq.com>
2021-11-11 18:28:22 +0800

    11.11 练习七的递归查询所有文件及文件夹

---
+++ JavaHomework/src/Exe7_1.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ * @Author Gallon
+ * @ClassName Exe7_1
+ * @Description  递归显示文件夹内所有文件以及子文件夹的名字
+ * @Time 2021-11-11,周四 18:11
+ */
+
+public class Exe7_1 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        String path = sc.next();
+        File target=new File(path);
+        if(!target.exists()) throw new Exception("目标文件夹不存在");
+        if(target.isFile()) throw new Exception("目标为文件，不可递归");
+        search(target);
+
+    }
+    static public void search(File target){
+        if(target.isFile()) System.out.println(target.getName());
+        else{
+            System.out.println(target.getName());//文件夹的名字也要打印
+            //递归打印的顺序类似于树遍历方法中的先序遍历。
+            File[] files=target.listFiles();
+            for (File file:files
+                 ) {
+                search(file);
+            }
+        }
+    }
+}
