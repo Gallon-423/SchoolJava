6f6fd867b40f7395b72698cbce1c2cc1fc5f8b4b
ChenJialong <2833275565@qq.com>
2021-11-23 16:37:08 +0800

    11.23 这一周的Java上机作业

--- JavaHomework/src/Exe8_3.java
+++ JavaHomework/src/Exe8_3.java
@@ -4,7 +4,8 @@ import java.awt.event.*;
 /**
  * @Author Gallon
  * @ClassName Exe8_3
- * @Description  鼠标事件点击切换幻灯片，红绿蓝播放
+ * @Description  鼠标事件点击（点击左键右键均可）切换幻灯片，红绿蓝播放
+ * 左键点击切换顺序为：红绿蓝（正序）， 右键点击切换顺序为：蓝绿红（逆序）。
  * @Time 2021-11-23,周二 16:28
  */
 
@@ -24,6 +25,12 @@ public class Exe8_3 {
             {
                 if(e.getButton() == MouseEvent.BUTTON1){
                     i[0]++;
+                    if ((i[0] % 3) == 0) jPanel.setBackground(Color.red);
+                    else if ((i[0] % 3) == 1) jPanel.setBackground(Color.green);
+                    else if ((i[0] % 3) == 2) jPanel.setBackground(Color.blue);
+                }else if(e.getButton() == MouseEvent.BUTTON3){
+                    i[0]--;
+                    if (i[0]<0) i[0]=i[0]+3;
                     if ((i[0] % 3) == 0) jPanel.setBackground(Color.red);
                     else if ((i[0] % 3) == 1) jPanel.setBackground(Color.green);
                     else if ((i[0] % 3) == 2) jPanel.setBackground(Color.blue);
---
+++ JavaHomework/src/Exe8_4.java
@@ -0,0 +1,70 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+/**
+ * @Author Gallon
+ * @ClassName Exe8_4
+ * @Description  创建用户登录界面
+ * @Time 2021-11-23,周二 16:29
+ */
+
+public class Exe8_4 {
+    private static void createGUI(){
+        MyFrame myFrame=new MyFrame();
+        myFrame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                createGUI();
+            }
+        });
+    }
+}
+
+class MyFrame extends JFrame implements ActionListener{
+
+    JPanel panel;
+    JLabel label,label2;
+    JButton MyFrameButton;
+    JTextField jTextField1;
+    JTextField jTextField2;
+    JPasswordField passwordField;
+    public MyFrame() {
+        this.setTitle("用户登录界面");
+        this.setSize(220,220);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        panel.setSize(200,200);
+        label = new JLabel("用户名");
+        label2 = new JLabel("密 码");
+        MyFrameButton = new JButton("登录");
+        MyFrameButton.addActionListener(this);
+        jTextField1 = new JTextField(12);
+        jTextField2 = new JTextField(12);
+        panel.add(label);
+        panel.add(jTextField1);
+        panel.add(label2);
+        panel.add(jTextField2);
+
+        panel.add(MyFrameButton);
+        this.add(panel);
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()==MyFrameButton) {
+            if (jTextField1.getText().equals("JavaUser") && jTextField2.getText().equals("Java1234")) {
+                JOptionPane.showMessageDialog(null,"登录成功！");
+            }else {
+                JOptionPane.showMessageDialog(null,"登录失败！");
+            }
+
+        }
+    }
+}
